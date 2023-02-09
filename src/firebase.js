cd58f7c2a7a0c3952bd149cded58bf03c819fbfc
Waqar Ahmed <waqarhafeez12@gmail.com>
2023-02-09 13:49:33 +0500

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeBVIT8t1LUE5OMY7-QndEC4KKesVs818",
+  authDomain: "netflix-build-307b4.firebaseapp.com",
+  projectId: "netflix-build-307b4",
+  storageBucket: "netflix-build-307b4.appspot.com",
+  messagingSenderId: "717252425519",
+  appId: "1:717252425519:web:34d1e271f300b7ac5493da",
+};
